746f2a1a38cfa3f8314e25cbfc28991d8d6cb6d5
Abhas kumar <85804879+abhas101@users.noreply.github.com>
2023-02-11 01:20:34 +0530

    v-1 Dynamic Animated portfolio
    Tried Many new features . Responsive,darkmode,animated, real form submit, consistant

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+// dark mode setup
+let moonIcon = document.getElementById('moonIcon');
+let body = document.getElementById('body');
+moonIcon.onclick = ()=>{
+    moonIcon.classList.toggle('bx-sun');
+    body.classList.toggle('darkMode');
+}
+// dark mode setup end
+
+  
