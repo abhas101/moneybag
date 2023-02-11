268eec07ba317f15037035ce696dda4abc841b14
abhas101 <abhaskumarmastermind@gmail.com>
2023-02-11 22:54:01 +0530

    v-1.1.1-Major

--- README.md
+++ README.md
@@ -3,3 +3,20 @@ Dynamic Portfolio Website Project
 Created By Abhas kumar
 Used Inspiration online from Beedim code and made the site more responsive with a custom touch
 Thanks.
+
+
+=======================
+changelog
+=======================
+v-1.1.0
+Added Dark Mode toggle
+
+=======================
+v-1-1-1
+
+Dark Mode -Animated Icon
+Dark Mode color Change
+Image Parallex Effect for Depth
+Change in Card and footer color
+=======================
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,26 @@ let moonIcon = document.getElementById('moonIcon');
 let body = document.getElementById('body');
 moonIcon.onclick = ()=>{
     moonIcon.classList.toggle('bx-sun');
+    // moonIcon.classList.toggle('bx-spin');
     body.classList.toggle('darkMode');
+    if(body.className==='darkMode'){
+        moonIcon.style.color = 'yellow';
+    }
+    else{
+        moonIcon.style.color ='black';
+    }
 }
+
 // dark mode setup end
 
-  
+// image animation start
+let img = document.getElementById('heroPhoto');
+let animated = document.getElementById('animatedIconsContainer');
+let cardBack = document.getElementById('cardBack');
+
+window.addEventListener('scroll',()=>{
+    let val = window.scrollY;
+    img.style.marginTop = val*-0.11 + 'px';
+})
+
+// image animation end
